8893560fbb7f3175d24507450173fcc15ec50983
nersus15 <38497343+nersus15@users.noreply.github.com>
2020-06-16 20:02:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Default index directory
-app.use(
-    express.static(
-        path.resolve(__dirname, "dist/"),
-        {index: "userouterinstead"}
-    )
-)
-
 // send the user to index html page inspite of the url
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+  res.direct('/dist/);
+   
 })
 
 app.listen(port);
