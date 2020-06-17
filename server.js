a972f52681a7606d9506c917056a2661bffa7f30
nersus15 <38497343+nersus15@users.noreply.github.com>
2020-06-17 09:18:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // send the user to index html page inspite of the url
-
+app.get('*', (req, res)=>{
+    res.sendFile(path.joiin(__diname, 'dist/index.html'));
+})
 
 app.listen(port);
