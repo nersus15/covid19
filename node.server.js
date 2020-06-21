90eb7f83204f7e5fb8dfce0adc60d196d3bb2bc1
nersus15 <38497343+nersus15@users.noreply.github.com>
2020-06-21 11:35:30 +0800

    Update node.server.js

--- node.server.js
+++ node.server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + 'dist/'));
+app.use(express.static(__dirname + '/dist/'));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res)=>{
